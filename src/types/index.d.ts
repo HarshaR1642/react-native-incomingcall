a2f5d47e016db4ec48ff97500414e025e83c2cfc
HarshaR1642 <sriharsha@rently.com>
2025-01-03 16:15:50 +0530

    remove types

--- src/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'react-native-incomingcall' {
-  export function registerReceiver(): void;
-  export function unregisterReceiver(): void;
-  export function showIncomingCall(): void;
-  export function endCall(): void;
-  export function areNotificationsEnabled(): Promise<boolean>;
-}
